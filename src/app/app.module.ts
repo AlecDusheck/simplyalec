6f9cf5904953b06792a959a3a51ed69ae1fe8eb7
alec@simplyalec.com <alec@simplyalec.com>
2018-09-13 18:13:27 -0500

    Sentry.io for errors, cleanup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ErrorHandler, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +13,18 @@ import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { NotFoundComponent } from './not-found/not-found.component';
+
+import * as Raven from 'raven-js';
+
+Raven
+  .config('https://eb41f92dc87440919cf8f10b8f440efd@sentry.io/1277499')
+  .install();
+
+export class RavenErrorHandler implements ErrorHandler {
+  handleError(err:any) : void {
+    Raven.captureException(err);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -31,7 +43,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: RavenErrorHandler}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
